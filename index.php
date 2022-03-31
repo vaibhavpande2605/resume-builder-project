afc4eb153e5b61827f68c5e42c2f368eebc25e95
Vaibhav Pande <91242365+vaibhavpande2605@users.noreply.github.com>
2022-03-31 08:39:37 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resume-Builder | Homepage</title>
+</head>
+<body>
+    <h1>Hello world</h1>
+    <a href="1/index.php">Basic Resume</a> <br>
+    <a href="2/index.php">Modern Resume</a> <br>
+    <a href="3/index.php">Standard Resume</a> <br>
+
+    <a href="login.php">Login</a> <br>
+    <a href="register.php">Register</a> <br>
+    <a href="logout.php">Logout</a> <br>
+</body>
+</html>
